be43087fc68cc7e89f94f07f9b125a49271a2687
Santosh Mardi <gsantosh@codeaurora.org>
2012-07-26 18:48:05 +0530

    ASoC: msm: Add AIO support for multi channel pcm platform driver.
    Add Async write mechanism for multichannel PCM platform driver.
    This will facilitate to have meta data information flow from apps
    To DSP and vice versa.
    
    Signed-off-by: Santosh Mardi <gsantosh@codeaurora.org>
    (cherry picked from commit 69f2b8ac7106fe5d5fa19ee12131c27efc8e5ec1)
    (cherry picked from commit 45e605da5fd57e07609657c0fac43853d3d0658e)
    
    Change-Id: I6a931b4458f14b64040eb7502aee26250938ba9d
    Signed-off-by: Sudhir Sharma <sudsha@codeaurora.org>
    
    Conflicts:
    	sound/soc/msm/msm-multi-ch-pcm-q6.c

--- sound/soc/msm/msm-multi-ch-pcm-q6.c
+++ sound/soc/msm/msm-multi-ch-pcm-q6.c
@@ -195,45 +195,28 @@ static void event_handler(uint32_t opcode,
 				atomic_set(&prtd->start, 1);
 				break;
 			}
-			if (prtd->mmap_flag) {
-				pr_debug("%s:writing %d bytes"\
-					" of buffer[%d] to dsp\n",
-					__func__, prtd->pcm_count,
-					prtd->out_head);
-				buf = prtd->audio_client->port[IN].buf;
-				pr_debug("%s:writing buffer[%d] from 0x%08x\n",
-					__func__, prtd->out_head,
-					((unsigned int)buf[0].phys
-					+ (prtd->out_head * prtd->pcm_count)));
-				param.paddr = (unsigned long)
-						buf[prtd->out_head].phys;
-				param.len = prtd->pcm_count;
-				param.msw_ts = 0;
-				param.lsw_ts = 0;
-				param.flags = NO_TIMESTAMP;
-				param.uid =  (unsigned long)
-						buf[prtd->out_head].phys;
-				if (q6asm_async_write(prtd->audio_client,
-							&param) < 0)
-					pr_err("%s:q6asm_async_write failed\n",
-						__func__);
-				else
-					prtd->out_head =
-						(prtd->out_head + 1)
-						& (runtime->periods - 1);
-			} else {
-				while (atomic_read(&prtd->out_needed)) {
-					pr_debug("%s:writing %d bytesi" \
-						  " of buffer to dsp\n", \
-						__func__, \
-						prtd->pcm_count);
-					q6asm_write_nolock(prtd->audio_client,
-						prtd->pcm_count,
-						0, 0, NO_TIMESTAMP);
-					atomic_dec(&prtd->out_needed);
-					wake_up(&the_locks.write_wait);
-				};
-			}
+			pr_debug("%s:writing %d bytes"\
+				" of buffer[%d] to dsp\n",
+				__func__, prtd->pcm_count, prtd->out_head);
+			buf = prtd->audio_client->port[IN].buf;
+			pr_debug("%s:writing buffer[%d] from 0x%08x\n",
+				__func__, prtd->out_head,
+				((unsigned int)buf[0].phys
+				+ (prtd->out_head * prtd->pcm_count)));
+			param.paddr = (unsigned long)buf[prtd->out_head].phys;
+			param.len = prtd->pcm_count;
+			param.msw_ts = 0;
+			param.lsw_ts = 0;
+			param.flags = NO_TIMESTAMP;
+			param.uid =  (unsigned long)buf[prtd->out_head].phys;
+			if (q6asm_async_write(prtd->audio_client,
+						&param) < 0)
+				pr_err("%s:q6asm_async_write failed\n",
+					__func__);
+			else
+				prtd->out_head =
+					(prtd->out_head + 1)
+					& (runtime->periods - 1);
 			atomic_set(&prtd->start, 1);
 			break;
 		default:
@@ -755,6 +738,12 @@ static int msm_pcm_hw_params(struct snd_pcm_substream *substream,
 		dir = IN;
 	else
 		dir = OUT;
+
+	ret = q6asm_set_io_mode(prtd->audio_client, ASYNC_IO_MODE);
+	if (ret < 0) {
+		pr_err("%s: Set IO mode failed\n", __func__);
+		return -ENOMEM;
+	}
 
 	ret = q6asm_audio_client_buf_alloc_contiguous(dir,
 		prtd->audio_client,
