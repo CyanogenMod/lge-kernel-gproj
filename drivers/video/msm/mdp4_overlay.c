cceb6b5bc6378eca3398bbce1843abcccc900f47
Naseer Ahmed <naseer@codeaurora.org>
2012-11-09 18:57:33 -0500

    msm_fb: display: Enable scaling bit while setting x-flip bit
    To enable left-right flip x-scaling bit should be set for
    MDP 4.2 and above versions. This fixes the issue when RGB layer
    pipes are not properly rotated.
    
    Change-Id: I0afae4aecde39ce3ac4dcbefc0bda93510e7d18a
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay.c

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -749,13 +749,11 @@ void mdp4_overlay_rgb_setup(struct mdp4_overlay_pipe *pipe)
 		op_mode &= ~(MDP4_OP_FLIP_UD + MDP4_OP_SCALEY_EN);
 		outpdw(rgb_base + 0x0058, op_mode);/* MDP_RGB_OP_MODE */
 	} else {
-//[S_QCT_PATCH][01009159] patch for pre-rotation	
 		if (pipe->op_mode & MDP4_OP_FLIP_LR && mdp_rev >= MDP_REV_42) {
 			/* Enable x-scaling bit to enable LR flip */
 			/* for MDP > 4.2 targets */
 			pipe->op_mode |= 0x01;
 		}
-//[E_QCT_PATCH]		
 		outpdw(rgb_base + 0x0058, pipe->op_mode);/* MDP_RGB_OP_MODE */
 	}
 	outpdw(rgb_base + 0x005c, pipe->phasex_step);
