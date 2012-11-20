e6537c1d71be521c911770207efa987c4e704e24
Naseer Ahmed <naseer@codeaurora.org>
2012-11-19 19:22:02 -0500

    msm_fb: display: Consolidate commit ioctls
    Overlay commit and display commit were two separate
    ioctls doing similar things. This change consolidates
    the two into one display commit ioctl. With this, we
    can use the RGB0 pipe with an IOMMU framebuffer.
    
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mdp4.h
    	drivers/video/msm/mdp4_overlay.c
    	drivers/video/msm/msm_fb.c
    	include/linux/msm_mdp.h

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -3614,12 +3614,18 @@ int mdp4_overlay_commit(struct fb_info *info)
 {
 	int ret = 0;
 	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)info->par;
+	int mixer;
 
 	if (mfd == NULL)
 		return -ENODEV;
 
 	if (!mfd->panel_power_on) /* suspended */
 		return -EINVAL;
+
+	mixer = mfd->panel_info.pdest;	/* DISPLAY_1 or DISPLAY_2 */
+
+	if (mixer >= MDP4_MIXER_MAX)
+		return -EPERM;
 
 	mutex_lock(&mfd->dma->ov_mutex);
 
