9afe9e0d11d12b1cf3c9d9fd86a88250a565054c
Ken Zhang <kenz@codeaurora.org>
2012-12-17 10:35:15 -0500

    msm: display: maintain smmu for secure video
    Currently video maps and unmaps the secure memory.
    It could be too early to unmap it.
    Rotator and MDP will maintain this, only unmap it when
    no more needed. Memory managr has a refence count for it.
    
    Change-Id: I5ca7cb09c0ad4b9e39808e937c2f50264727730c
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>
    
    Conflicts:
    	drivers/char/msm_rotator.c
    	drivers/video/msm/mdp4.h

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -2218,6 +2218,9 @@ static int msm_fb_pan_display_sub(struct fb_var_screeninfo *var,
 			     (var->activate & FB_ACTIVATE_VBL));
 	mdp_dma_pan_update(info);
 	msm_fb_signal_timeline(mfd);
+	if (mdp4_unmap_sec_resource())
+		pr_err("%s: unmap secure res failed\n", __func__);
+
 	up(&msm_fb_pan_sem);
 
 #if defined(CONFIG_MACH_LGE)
