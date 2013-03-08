4b9c8bd471068b91d83f5bcc1d4cfc8f10262928
Carter Cooper <ccooper@codeaurora.org>
2013-03-08 14:59:36 -0800

    msm: kgsl: Remove all reference to CONFIG_ANDROID_PMEM
    CONFIG_ANDROID_PMEM is completely deprecated. Remove all
    references to it.
    
    Change-Id: I34039709b0dc6cd5bb96497a8df971f420836023
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>
    
    Conflicts:
    	drivers/gpu/msm/kgsl.c

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -19,7 +19,6 @@
 #include <linux/uaccess.h>
 #include <linux/interrupt.h>
 #include <linux/workqueue.h>
-#include <linux/android_pmem.h>
 #include <linux/vmalloc.h>
 #include <linux/pm_runtime.h>
 #include <linux/genlock.h>
@@ -2258,11 +2257,10 @@ static int kgsl_get_phys_file(int fd, unsigned long *start, unsigned long *len,
 	dev_t rdev;
 	struct fb_info *info;
 
+	*start = 0;
+	*vstart = 0;
+	*len = 0;
 	*filep = NULL;
-#ifdef CONFIG_ANDROID_PMEM
-	if (!get_pmem_file(fd, start, vstart, len, filep))
-		return 0;
-#endif
 
 	fbfile = fget(fd);
 	if (fbfile == NULL) {
@@ -2353,9 +2351,6 @@ static int kgsl_setup_phys_file(struct kgsl_mem_entry *entry,
 
 	return 0;
 err:
-#ifdef CONFIG_ANDROID_PMEM
-	put_pmem_file(filep);
-#endif
 	return ret;
 }
 
