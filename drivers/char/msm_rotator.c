fc089eee4ffb77f481a2abfea1b736b5703d4ad3
Kuogee Hsieh <khsieh@codeaurora.org>
2013-03-27 17:41:49 -0700

    msm_fb: display: exit properly when rotator max session reached
    When rotator session reach maximum, it should exit without calling
    put_img since no get_img had been called.
    
    CRs-fixed: 466462
    Change-Id: I951570227ebd81da6dad19c1b897cde84f8d0451
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/char/msm_rotator.c
+++ drivers/char/msm_rotator.c
@@ -1319,7 +1319,8 @@ static int msm_rotator_rotate_prepare(
 		pr_err("%s() : Attempt to use invalid session_id %d\n",
 			__func__, s);
 		rc = -EINVAL;
-		goto do_rotate_unlock_mutex;
+		mutex_unlock(&msm_rotator_dev->rotator_lock);
+		return rc;
 	}
 
 	img_info = &(msm_rotator_dev->rot_session[s]->img_info);
@@ -1327,7 +1328,8 @@ static int msm_rotator_rotate_prepare(
 		dev_dbg(msm_rotator_dev->device,
 			"%s() : Session_id %d not enabled\n", __func__, s);
 		rc = -EINVAL;
-		goto do_rotate_unlock_mutex;
+		mutex_unlock(&msm_rotator_dev->rotator_lock);
+		return rc;
 	}
 
 	if (msm_rotator_get_plane_sizes(img_info->src.format,
@@ -1336,7 +1338,8 @@ static int msm_rotator_rotate_prepare(
 					&src_planes)) {
 		pr_err("%s: invalid src format\n", __func__);
 		rc = -EINVAL;
-		goto do_rotate_unlock_mutex;
+		mutex_unlock(&msm_rotator_dev->rotator_lock);
+		return rc;
 	}
 	if (msm_rotator_get_plane_sizes(img_info->dst.format,
 					img_info->dst.width,
@@ -1344,7 +1347,8 @@ static int msm_rotator_rotate_prepare(
 					&dst_planes)) {
 		pr_err("%s: invalid dst format\n", __func__);
 		rc = -EINVAL;
-		goto do_rotate_unlock_mutex;
+		mutex_unlock(&msm_rotator_dev->rotator_lock);
+		return rc;
 	}
 
 	rc = get_img(&info.src, ROTATOR_SRC_DOMAIN, (unsigned long *)&in_paddr,
