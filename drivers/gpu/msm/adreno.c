ef0fd1b5259817718789afdc90bd756c8bfa7a02
Sakshi Agrawal <sakshia@codeaurora.org>
2013-04-12 17:52:48 -0600

    msm: kgsl: Always fire an interupt if requested
    Even if the event code specifies a dummy interupt it might still
    get wrapped with the conditional check.  Make sure that all forced
    interrupts from the event code get fired.
    
    Change-Id: Ic0dedbadc4599efcbca65ad93cc27943bad2ca79
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>
    Signed-off-by: Tarun Karra <tkarra@codeaurora.org>
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Sakshi Agrawal <sakshia@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -2643,7 +2643,7 @@ static unsigned int adreno_check_hw_ts(struct kgsl_device *device,
 
 		if (context) {
 			adreno_ringbuffer_issuecmds(device, context->devctxt,
-					KGSL_CMD_FLAGS_NONE, NULL, 0);
+					KGSL_CMD_FLAGS_GET_INT, NULL, 0);
 		}
 	}
 
