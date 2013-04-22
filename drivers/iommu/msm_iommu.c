cb15d72d0a8c272ff44ef9d88579fa32b93860f3
Laura Abbott <lauraa@codeaurora.org>
2013-04-22 10:01:55 -0700

    iommu: msm: Don't treat address 0 as an error case
    Currently, the iommu page table code treats a scattergather
    list with physical address 0 as an error. This may not be
    correct in all cases. Physical address 0 is a valid part
    of the system and may be used for valid page allocations.
    Nothing else in the system checks for physical address 0
    for error so don't treat it as an error.
    
    Change-Id: Ie9f0dae9dace4fff3b1c3449bc89c3afdd2e63a0
    CRs-Fixed: 478304
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/iommu/msm_iommu.c
+++ drivers/iommu/msm_iommu.c
@@ -1,5 +1,4 @@
-/* Copyright (c) 2010-2012, The Linux Foundation. All rights reserved.
- *
+/* Copyright (c) 2010-2012, The Linux Foundation. All rights reserved.  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
  * only version 2 as published by the Free Software Foundation.
@@ -49,6 +48,9 @@ __asm__ __volatile__ (							\
 #define MSM_IOMMU_ATTR_CACHED_WB_NWA	0x2
 #define MSM_IOMMU_ATTR_CACHED_WT	0x3
 
+
+static int msm_iommu_unmap_range(struct iommu_domain *domain, unsigned int va,
+				 unsigned int len);
 
 static int msm_iommu_unmap_range(struct iommu_domain *domain, unsigned int va,
 				 unsigned int len);
@@ -914,6 +916,7 @@ static int msm_iommu_map_range(struct iommu_domain *domain, unsigned int va,
 			       int prot)
 {
 	unsigned int pa;
+	unsigned int start_va = va;
 	unsigned int offset = 0;
 	unsigned long *fl_table;
 	unsigned long *fl_pte;
@@ -985,12 +988,6 @@ static int msm_iommu_map_range(struct iommu_domain *domain, unsigned int va,
 				chunk_offset = 0;
 				sg = sg_next(sg);
 				pa = get_phys_addr(sg);
-				if (pa == 0) {
-					pr_debug("No dma address for sg %p\n",
-							sg);
-					ret = -EINVAL;
-					goto fail;
-				}
 			}
 			continue;
 		}
@@ -1044,12 +1041,6 @@ static int msm_iommu_map_range(struct iommu_domain *domain, unsigned int va,
 				chunk_offset = 0;
 				sg = sg_next(sg);
 				pa = get_phys_addr(sg);
-				if (pa == 0) {
-					pr_debug("No dma address for sg %p\n",
-							sg);
-					ret = -EINVAL;
-					goto fail;
-				}
 			}
 		}
 
@@ -1062,6 +1053,8 @@ static int msm_iommu_map_range(struct iommu_domain *domain, unsigned int va,
 	__flush_iotlb(domain);
 fail:
 	mutex_unlock(&msm_iommu_lock);
+	if (ret && offset > 0)
+		msm_iommu_unmap_range(domain, start_va, offset);
 	return ret;
 }
 
