e03046315c12f0c9b26f403c56fec5a5545bd384
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-05-23 14:57:45 +0100

    Revert "msm: kgsl: Add GFP_NORETRY to the page_alloc flags and handle errors"
    This reverts commit 6e06132cf868b2dea049447cefced1a0656dd7ba.

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -576,8 +576,7 @@ _kgsl_sharedmem_page_alloc(struct kgsl_memdesc *memdesc,
 		 * range ourselves (see below)
 		 */
 
-		pages[i] = alloc_page(GFP_KERNEL | __GFP_HIGHMEM |
-			__GFP_NOWARN | __GFP_NORETRY);
+		pages[i] = alloc_page(GFP_KERNEL | __GFP_HIGHMEM);
 		if (pages[i] == NULL) {
 			ret = -ENOMEM;
 			memdesc->sglen = i;
