8fd495c9d050b3cceef950a85050c8872f3528d4
Mr-MEX <binder_z@mail.ru>
2015-05-04 13:20:58 +0400

    make menuconfig: Added option gpu overclock

--- arch/arm/mach-msm/lge/j1/board-j1-gpu.c
+++ arch/arm/mach-msm/lge/j1/board-j1-gpu.c
@@ -232,6 +232,7 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
+#ifdef CONFIG_GPU_OVCL
 		{
 			.gpu_freq = 622000000,
 			.bus_freq = 9,
@@ -257,6 +258,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.bus_freq = 5,
 			.io_fraction = 0,
 		},
+#endif
 		{
 			.gpu_freq = 400000000,
 			.bus_freq = 4,
@@ -283,7 +285,11 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 1,
+#ifdef CONFIG_GPU_OVCL
 	.num_levels = 10,
+#else
+	.num_levels = 5,
+#endif
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.strtstp_sleepwake = true,
@@ -313,7 +319,11 @@ void __init apq8064_init_gpu(void)
 	unsigned int version = socinfo_get_version();
 
 	if (cpu_is_apq8064ab())
+#ifdef CONFIG_GPU_OVCL
 		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 622000000;
+#else
+		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 400000000;
+#endif
 	if (SOCINFO_VERSION_MAJOR(version) == 2) {
 		kgsl_3d0_pdata.chipid = ADRENO_CHIPID(3, 2, 0, 2);
 	} else {
